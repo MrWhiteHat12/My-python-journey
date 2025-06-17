5f87ccc0d39177f320e5ce565044766d24f71acb
MrWhiteHat12 <143561223+MrWhiteHat12@users.noreply.github.com>
2025-06-17 12:00:34 -0600

    refactor spiral drawing logic to use dynamic dot size and spacing

--- pygame_turple_learning/Turple_V1.py
+++ pygame_turple_learning/Turple_V1.py
@@ -7,33 +7,40 @@ dan = Turtle()
 dan.shape("turtle")
 screen = Screen()
 screen.bgcolor("#F5F0E0")
-screen.colormode
 dan.hideturtle()
 screen.screensize(50, 50)
 dan.penup()
 dan.setposition(-255, -255)
-color_lib.make_mod_color("#F5F0E0", 50)
-a = 10
-for i in range(3):
-    for i in range(a):
-        dan.dot(20, random.choice(color_lib.mod_color))
-        dan.forward(50)
-    dan.left(90)
-a -= 1
-for i in range(5):
-    for i in range(2):
-        for i in range(a):
-            dan.dot(20, random.choice(color_lib.mod_color))
-            dan.forward(50)
-        dan.left(90)
-    a -= 1
-    for i in range(2):
-        for i in range(a):
-            dan.dot(20, random.choice(color_lib.mod_color))
-            dan.forward(50)
-        dan.left(90)
-    a -= 1
 
-dan.dot(20, random.choice(color_lib.mod_color))
+
+def make_spiral(size_of_dot, backround, too_close):
+    color_lib.make_mod_color(backround, too_close)
+    inbetween = int(size_of_dot*2.5)
+    a = int(495 / (size_of_dot + inbetween / 2) - 1)
+    for i in range(3):
+        for i in range(a):
+            dan.dot(size_of_dot, random.choice(color_lib.mod_color))
+            dan.forward(inbetween)
+        dan.left(90)
+    a -= 1
+    for i in range(10):
+        for i in range(2):
+            for i in range(a):
+                dan.dot(size_of_dot, random.choice(color_lib.mod_color))
+                dan.forward(inbetween)
+            dan.left(90)
+        a -= 1
+    for i in range(2):
+        for i in range(a):
+            dan.dot(size_of_dot, random.choice(color_lib.mod_color))
+            dan.forward(inbetween)
+        dan.left(90)
+        a -= 1
+
+    dan.dot(size_of_dot, random.choice(color_lib.mod_color))
+
+
+how_big = input("how big do you want the dot to be? ")
+make_spiral(how_big, "#F5F0E0", 50)
 
 screen.exitonclick()

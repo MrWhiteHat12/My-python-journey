2a39931adbda7db256ad4b5963115f38aee1fbf8
MrWhiteHat12 <143561223+MrWhiteHat12@users.noreply.github.com>
2025-06-18 10:09:58 -0600

    fix: adjust turtle width and loop count for shape drawing

--- pygame_turple_learning/turtle_shapes_V1.py
+++ pygame_turple_learning/turtle_shapes_V1.py
@@ -7,7 +7,7 @@ dan = Turtle()
 dan.shape("turtle")
 screen = Screen()
 screen.bgcolor("#F5F0E0")
-# dan.hideturtle()
+dan.hideturtle()
 screen.screensize(50, 50)
 
 
@@ -15,9 +15,9 @@ turn = 360
 num_of_turn = 3
 value_of_turn = turn // num_of_turn
 distance = 50
-dan.width(15)
+dan.width(5)
 
-for i in range(5):
+for i in range(4):
     color_lib.make_mod_color("#F5F0E0", 50)
     dan.pencolor(random.choice(color_lib.mod_color))
     for i in range(num_of_turn):
